8005345c037772129fc8fcd5633fc3773e0d1338
nereo.lopez <nereo.lopez@outlook.com>
2018-04-09 18:49:33 +0200

    WebElements Class

---
+++ SeleniumCsharpIntro/WebElementExercises.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Diagnostics;
+
+namespace SeleniumCsharpIntro
+{
+    [TestClass]
+    public class WebElementExercises
+    {
+        IWebDriver driver;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [TestMethod]
+        public void WebElementExercise1Test()
+        {
+            string url = "http://book.theautomatedtester.co.uk/chapter1";
+            this.driver.Url = url;
+
+            // 1. Check the radio button
+            var radioButton = this.driver.FindElement(By.Id("radiobutton"));
+            Assert.IsFalse(radioButton.Selected);
+            radioButton.Click();
+            Assert.IsTrue(radioButton.Selected);
+
+            // 2. Dropdown
+            var defaultDropdownExpectedTet = "Selenium IDE";
+            var wantedSelectedOption = "Selenium Grid";
+            var dropdown = new SelectElement(this.driver.FindElement(By.TagName("select")));
+            Assert.AreEqual(dropdown.SelectedOption.Text, defaultDropdownExpectedTet);
+            dropdown.SelectByValue(wantedSelectedOption);
+            Assert.AreEqual(dropdown.SelectedOption.Text, wantedSelectedOption);
+
+            // 3. Navigate and check element availability
+            var homePageLink = this.driver.FindElement(By.LinkText("Home Page"));
+            homePageLink.Click();
+            var chapter1Link = this.driver.FindElement(By.LinkText("Chapter1"));
+            Assert.AreEqual(chapter1Link.Text, "Chapter1");
+
+            // 4. Navigate Back
+            this.driver.Navigate().Back();
+
+            // 5. Check Checkbox besides Dropdown.
+            var checkbox = this.driver.FindElement(By.Name("selected(1234)"));
+            Assert.IsFalse(checkbox.Selected);
+            checkbox.Click();
+            Assert.IsTrue(checkbox.Selected);
+
+            // 6. Assert Text on the Page
+            var textParent = this.driver.FindElement(By.ClassName("leftdiv"));
+            Assert.AreEqual(textParent.Text, "Assert that this text is on the page");
+        }
+
+        [TestMethod]
+        public void WebElementExercise2Test()
+        {
+            string homePageUrl = "https://grup5web.firebaseapp.com/";
+            string propertiesListUrl = "https://grup5web.firebaseapp.com/properties/properties.html?region=Navarra";
+
+            // 1. Navigate first to homePageURL to let the app populate the local storage with properties
+            this.driver.Url = homePageUrl;
+            this.driver.Url = propertiesListUrl;
+
+            // 2.Get Property Info
+            var propertyCard = this.driver.FindElement(By.Id("card-1"));
+            var price = propertyCard.FindElement(By.TagName("h2")).Text;
+            var bedrooms = propertyCard.FindElement(By.Id("rooms-1")).Text.Split(' ')[0];
+            var interestedPeople = propertyCard.FindElement(By.ClassName("likes")).Text.Split(' ')[0];
+            Debug.WriteLine(price);
+            Debug.WriteLine(bedrooms);
+            Debug.WriteLine(interestedPeople);
+
+            // 3. Go to details view and assert data consistency
+            propertyCard.Click();
+            var detailsPrice = this.driver.FindElement(By.Id("price")).Text.Replace(" ", string.Empty);
+            var detailsBedrooms = this.driver.FindElement(By.Id("numberOfBedrooms")).Text.Split(' ')[0];
+
+            var likesElement = this.driver.FindElement(By.Id("likes"));
+            var detailsInterestedPeople = likesElement.Text;
+
+            Assert.AreEqual(price, detailsPrice);
+            Assert.AreEqual(bedrooms, detailsBedrooms);
+            Assert.AreEqual(interestedPeople, detailsInterestedPeople);
+
+            // 4. Send a Comment
+            var commentsTextArea = this.driver.FindElement(By.Id("comments"));
+            commentsTextArea.SendKeys("I am interested on this property!");
+            commentsTextArea.Submit();
+            Assert.AreEqual(Convert.ToInt32(detailsInterestedPeople) + 1, Convert.ToInt32(likesElement.Text));
+            string numberOfLikes = likesElement.Text; // To avoid having a stale element when navigating out of the page!
+
+            // 5. Navigate Back to Properties List
+            this.driver.Navigate().Back();
+
+            // 6. Check the new likes amount is correcet.
+            propertyCard = this.driver.FindElement(By.Id("card-1"));
+            interestedPeople = propertyCard.FindElement(By.ClassName("likes")).Text.Split(' ')[0];
+            Assert.AreEqual(interestedPeople, numberOfLikes);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            this.driver.Quit();
+        }
+    }
+}
---
+++ SeleniumCsharpIntro/WebElementSamples.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Diagnostics;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumCsharpIntro
+{
+    [TestClass]
+    public class WebElementSamples
+    {
+        IWebDriver driver;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [TestMethod]
+        public void WebElementTest()
+        {
+            IWebElement queryInput;
+            string url = "https://google.com";
+            this.driver.Url = url;
+
+            // BASIC OF FINDING ELEMENT
+            // Try the two below, one by one
+            queryInput = this.driver.FindElement(By.Id("lst-ib"));
+            // queryInput = this.driver.FindElement(By.Name("q"));
+
+            // CHECKING SOME IWEBELEMENT PROPERTIES
+            Assert.IsTrue(queryInput.Displayed);
+            Assert.IsTrue(queryInput.Enabled);
+            Assert.AreEqual(queryInput.TagName, "input");
+
+            Debug.WriteLine("Query Input size is {0} height and {1} width", queryInput.Size.Height, queryInput.Size.Width);
+            Debug.WriteLine("Query Input location is {0} horizontal {1} vertical", queryInput.Location.X, queryInput.Location.Y);
+
+            // USING SOME IWEBELEMENT METHODS
+            var queryText = "Automation wth Selenium and C#";
+            queryInput.SendKeys(queryText);
+            Assert.AreEqual(queryInput.GetAttribute("value"), queryText);
+
+            queryInput.Clear();
+            Assert.AreEqual(queryInput.GetAttribute("value"), "");
+
+            queryInput.SendKeys(queryText);
+            queryInput.Submit();
+
+            Assert.AreNotEqual(this.driver.Title, "Google");
+
+        }
+
+        [TestMethod]
+        public void SelectClassTest()
+        {
+            SelectElement filter;
+            // Sample web done by my wife with some University team mates :)
+            string url = "https://grup5web.firebaseapp.com/properties/properties.html?region=Navarra";
+            this.driver.Url = url;
+
+            // CHECKING SOME SELECTELEMENT PROPERTIES 
+            filter = new SelectElement(this.driver.FindElement(By.Id("filter")));
+            Assert.IsFalse(filter.IsMultiple);
+            Debug.WriteLine("These are the available options:");
+            foreach (var option in filter.Options)
+            {
+                Debug.WriteLine(option.Text);
+            }
+            Debug.WriteLine("DEFAULTED OPTION: " + filter.SelectedOption.Text);
+
+            int lastOptionIndex = filter.Options.Count - 1;
+            string lastOptionText = filter.Options[lastOptionIndex].Text;
+            filter.SelectByIndex(lastOptionIndex);
+
+            Assert.AreEqual(filter.SelectedOption.Text, lastOptionText);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            this.driver.Quit();
+        }
+    }
+}
